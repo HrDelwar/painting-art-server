fd91056819a83cce8cfbea0503ad5381c65b75a2
HrDelwar <hrdelwar75@gmail.com>
2021-09-16 16:27:43 +0600

    make mongodb uri as string

--- dbConnection.js
+++ dbConnection.js
@@ -6,7 +6,7 @@ const option = {
 };
 const dbConnection = () => {
   mongoose
-    .connect(process.env.MONGODB_URI, option)
+    .connect(process.env.MONGODB_URI.toString(), option)
     .then(() => {
       console.log("Yap! DB Connect successfully!");
     })
